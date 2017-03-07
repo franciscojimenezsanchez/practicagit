b9d791a30e8424b26bb63501002dff49ce81b7cb
FranciscoJimenez <franciscojimenezsanchez88@gmail.com>
2017-03-07 12:24:55 +0100

    cambios en master

--- Lib.java
+++ Lib.java
@@ -14,7 +14,7 @@ public class Lib {
     }
 
     public void metodoComunitario(){
-        System.out.println("Aqui escribimos todos");
+        System.out.println("cambio en master");
     }
 public  void  metodo3 (){
 System.out.println("metodo 3");
